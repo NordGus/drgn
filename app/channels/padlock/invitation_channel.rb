246770659f1c3c0a54ee89b2a84eac5b84287fe9
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2026-05-31 12:43:28 +0200

    moving broadcast logic to the channel on claimed

--- app/channels/padlock/invitation_channel.rb
+++ app/channels/padlock/invitation_channel.rb
@@ -2,6 +2,34 @@
 # Padlock::InvitationChannel is the ApplicationCable::StreamsChannel that controls all streams related to Padlock::Invitation
 # for real-time updates and asynchronous communications
 class Padlock::InvitationChannel < ApplicationCable::StreamsChannel
+  # Broadcasts the required UI changes when an invitation was claimed to all characters connected to the invitations
+  # settings panel to reflect the new platform state.
+  #
+  # @note This method should be called from a background job because this action could become a performance bottleneck,
+  #   if a deployment break the assumptions of our engineering tradeoff.
+  #
+  # @param invitation [Padlock::Invitation]
+  def self.broadcast_claimed(invitation)
+    # Using the BossKey::Recruiter feature we just need to broadcast that the invitation was claimed to the party members
+    # with access to the Invitations BossDoor.
+    #
+    # Because we are inside a background job, and because of our engineering tradeoff, this iteration does not represent
+    # a performance problem.
+    BossKey::Recruiter.with_whom_can_be_broadcasted.find_each do |key|
+      # We remove the invitation from the pending invitations list
+      broadcast_remove_to(key.holder, target: invitation)
+
+      # We prepend the claimed invitation to the accepted invitations list
+      broadcast_action_to(
+        key.holder,
+        action: :prepend,
+        target: "accepted_invitations",
+        partial: "settings/invitations/invitation",
+        locals: { invitation:, current_time: Time.current, current_character: key.holder }
+      )
+    end
+  end
+
   # Broadcasts the updated invitation to all characters connected to the invitations settings panel to replace it with
   # the new invitation state.
   #
--- app/jobs/padlock/invitation/on_claimed_job.rb
+++ app/jobs/padlock/invitation/on_claimed_job.rb
@@ -13,24 +13,8 @@ class Padlock::Invitation::OnClaimedJob < ApplicationJob
     return :no_invitation_received unless invitation.present?
     return :unclaimed_invitation_received unless invitation.claimed?
 
-    # Using the BossKey::Recruiter feature we just need to broadcast that the invitation was claimed to the party members
-    # with access to the Invitations BossDoor.
-    #
-    # Because we are inside a background job, and because of our engineering tradeoff, this iteration does not represent
-    # a performance problem.
-    BossKey::Recruiter.with_whom_can_be_broadcasted.find_each do |key|
-      # We remove the invitation from the pending invitations list
-      Padlock::InvitationChannel.broadcast_remove_to(key.holder, target: invitation)
-
-      # We prepend the claimed invitation to the accepted invitations list
-      Padlock::InvitationChannel.broadcast_action_to(
-        key.holder,
-        action: :prepend,
-        target: "accepted_invitations",
-        partial: "settings/invitations/invitation",
-        locals: { invitation:, current_time: Time.current, current_character: key.holder }
-      )
-    end
+    # We broadcast the state change to all channels that requires.
+    Padlock::InvitationChannel.broadcast_claimed(invitation)
 
     :claimed_invitation_processed
   end

0483cea2a6d722ebb8fe3d49b5ee33e4268cbf43
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2026-05-31 12:38:30 +0200

    cleaning code style

--- app/jobs/padlock/invitation/recurring/expire_orphans_job.rb
+++ app/jobs/padlock/invitation/recurring/expire_orphans_job.rb
@@ -3,7 +3,9 @@ class Padlock::Invitation::Recurring::ExpireOrphansJob < ApplicationJob
   limits_concurrency to: 1, key: :recurring_expire_orphans, duration: 1.minute, group: "PadlockActions"
 
   def perform(batch_size: 100)
-    Padlock::Invitation.tearable.find_each(batch_size:) { |invitation| Padlock::Invitation::OnExpiredJob.perform_later(invitation) }
+    Padlock::Invitation.tearable.find_each(batch_size:) do |invitation|
+      Padlock::Invitation::OnExpiredJob.perform_later(invitation)
+    end
 
     :all_orphan_invitations_expired
   end

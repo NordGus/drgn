ac9e8e83e8b1026b7a4325a735a7cb8569239e75
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2026-07-06 22:11:41 +0200

    adding on_not_configured_job event handler when checking post office whether it is configured or not

--- app/models/mechanic/post_office.rb
+++ app/models/mechanic/post_office.rb
@@ -1,17 +1,18 @@
 class Mechanic::PostOffice < Mechanic
   validates :type, inclusion: { in: %w[Mechanic::PostOffice] }
 
+  default_scope { includes(:address, :port, :username, :password) }
+
   has_one :address, class_name: "Mechanic::PostOffice::Address", foreign_key: :mechanic_id, dependent: :destroy
   has_one :port, class_name: "Mechanic::PostOffice::Port", foreign_key: :mechanic_id, dependent: :destroy
   has_one :username, class_name: "Mechanic::PostOffice::Username", foreign_key: :mechanic_id, dependent: :destroy
   has_one :password, class_name: "Mechanic::PostOffice::Password", foreign_key: :mechanic_id, dependent: :destroy
+  has_one :disable_notifications, class_name: "Mechanic::PostOffice::DisableNotifications", foreign_key: :mechanic_id, dependent: :destroy
 
   def configured?
     configured = address.present? && port.present? && username.present? && password.present?
 
-    unless configured
-
-    end
+    OnNotConfiguredJob.perform_later(Time.current) unless configured
 
     configured
   end
